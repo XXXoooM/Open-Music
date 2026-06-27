30118ea4d200a27f2149c5f05bd4c1c38e08f20e
XXXoooM <3437759510@qq.com>
2026-06-28 02:10:47 +0800

    fix: prevent Android WebView from pausing on background to keep background audio playing

--- android/app/src/main/java/com/openmusic/app/MainActivity.java
+++ android/app/src/main/java/com/openmusic/app/MainActivity.java
@@ -1,5 +1,29 @@
 package com.openmusic.app;
 
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+    @Override
+    protected void onPause() {
+        super.onPause();
+        // Force the WebView to resume execution in the background so that
+        // JavaScript events (e.g. ended, next song loading) continue to run.
+        if (this.bridge != null && this.bridge.getWebView() != null) {
+            this.bridge.getWebView().onResume();
+        }
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        // Disable the requirement for user gestures to start media playback
+        if (this.bridge != null && this.bridge.getWebView() != null) {
+            WebView webView = this.bridge.getWebView();
+            WebSettings settings = webView.getSettings();
+            settings.setMediaPlaybackRequiresUserGesture(false);
+        }
+    }
+}
